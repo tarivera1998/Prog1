efe102c608a6bc0530801420d26785fa368e06cd
tarivera1998 <tarivera1998@gmail.com>
2022-03-02 11:53:09 +0100

    Ch15 upload

---
+++ Drills/Ch15/drill15_1.cpp
@@ -0,0 +1,30 @@
+/*
+    g++ drill15_1.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill15_1 `fltk-config --ldflags --use-images`
+*/
+
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main ()
+{
+  using namespace Graph_lib;
+
+    int xmax = 600;
+    int ymax = 600;
+
+    int x_orig = xmax/2;
+    int y_orig = ymax/2;
+
+    int rmin = -11;
+    int rmax = 11;
+    
+    int n_points = 400;
+
+    Simple_window win {Point{100,100}, xmax, ymax, "Function graphs"};
+    
+    
+     win.wait_for_button();
+  }
+  
+  
+  
