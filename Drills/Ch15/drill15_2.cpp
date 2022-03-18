dca6a00bbcbba87757b25fb3d6503364482c0972
tarivera1998 <tarivera1998@gmail.com>
2022-03-18 21:16:48 +0100

    Upload class drill

--- Drills/Ch15/drill15_2.cpp
+++ Drills/Ch15/drill15_2.cpp
@@ -1,46 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
 #include "std_lib_facilities.h"
 
 
+using namespace std;
+
 struct Person {
-Person(): name(""), age(0) {}
-string name;
-int age;
+    Person() { }
+    Person(string f, string l, int a);
+
+    string first_name() const { return fn; }
+    string last_name() const { return ln; }
+    int age() const { return ag; }
 
 private:
-string first_name;
-string second_name;
-int age;
+    string fn;
+    string ln;
+    int ag;
+};
 
-}
-
-istream& operator>>(istream& is, Person& person)
+Person::Person(string f, string l, int a) : fn{f}, ln{l}, ag{a}
 {
-
-
-
-return is;
+    //dealing with possible errors
+    if (a < 0 || 150 < a) 
+    throw runtime_error("Invalid age used");
+    
+    string name = f + l;        
+    for (char c : name) {
+        switch(c) {
+            case ';': case ':': case '"': case '[': case ']': case '*':
+            case '&': case '^': case '%': case '$': case '#': case '@':
+            case '!':
+                throw runtime_error("Invalid character used");
+                break;
+            default:
+                break;
+        }
+    }
 }
 
-
-ostream& operator<<(ostream& os, const Person& person) //referencia szerinti átadás
+ostream& operator<<(ostream& os, const Person& p)
 {
-return os << "name: "<<person.name<<"\n"<<"age: "<<person.age;
-
+        return os << p.first_name() << " " << p.last_name() << "\t" << p.age();
 }
 
-int main () {
+istream& operator>>(istream& is, Person& p)
+{
+	string f;
+	string l;
+	int a;
 
-/*Person person {"Goofy", 63};
-cout << "name: "<< person.name <<"\n"<<"age: "<< person.age << "\n";*/
-
-
-
-/*cout << "Give me a name and an age: " << "\n";
-Person person1;
-cin >> person1;
-cout<< person1 <<"\n";*/
-
+	is >> f >> l >> a;
+    	p = Person(f, l, a);
+   	return is;
 }
 
-  
-  
+int main(){
+
+	/*Person person {"Goofy", 63};
+	cout << "name: "<< person.name <<"\n"<<"age: "<< person.age << "\n";*/
+
+
+
+	/*cout << "Give me a name and an age: " << "\n";
+	Person person1;
+	cin >> person1;
+	cout<< person1 <<"\n";*/
+    
+   	vector<Person> v;
+
+    	cout << "Enter first names, second names and ages. Exit with firstname and lastname 'end'\n";
+
+    	for (Person p; cin >> p; ) {
+        	if (p.first_name() == "end") break;
+        	v.push_back(p);
+    		}
+
+    	for (Person p : v)
+        cout << p << '\n';
+
+
+}
